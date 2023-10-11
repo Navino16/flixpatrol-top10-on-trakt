bb568484be8399b5f1112b893ec4b06906bf18e1
Nathan JAUNET <njaunet@datasolution.fr>
2023-10-11 17:31:25 +0200

    #7: Add popular lists

--- src/Utils/GetAndValidateConfigs.ts
+++ src/Utils/GetAndValidateConfigs.ts
@@ -1,44 +1,74 @@
 import config from 'config';
 import { TraktPrivacy } from 'trakt.tv';
-import { FlixPatrol, FlixPatrolLocation, FlixPatrolPlatform } from '../Flixpatrol';
+import { FlixPatrol, FlixPatrolTop10Location, FlixPatrolTop10Platform } from '../Flixpatrol';
 import { logger } from './Logger';
 
-export interface FlixPatrolConfig {
-  platform: FlixPatrolPlatform;
-  location: FlixPatrolLocation;
-  fallback: FlixPatrolLocation | false;
+export interface FlixPatrolTop10Config {
+  platform: FlixPatrolTop10Platform;
+  location: FlixPatrolTop10Location;
+  fallback: FlixPatrolTop10Location | false;
+  privacy: TraktPrivacy;
+}
+
+export interface FlixPatrolPopularConfig {
+  platform: FlixPatrolTop10Platform;
   privacy: TraktPrivacy;
 }
 
 export class GetAndValidateConfigs {
-  public static flixPatrol(): FlixPatrolConfig[] {
-    let flixPatrolConfigs: FlixPatrolConfig[] = [];
+  public static flixPatrolTop10(): FlixPatrolTop10Config[] {
+    let flixPatrolTop10Configs: FlixPatrolTop10Config[] = [];
     const traktPrivacy: string[] = ['private', 'link', 'friends', 'public'];
     try {
-      flixPatrolConfigs = config.get('FlixPatrol') as FlixPatrolConfig[];
-      flixPatrolConfigs.forEach((flixPatrolConfig) => {
-        if (!FlixPatrol.isFlixPatrolPlatform(flixPatrolConfig.platform)) {
-          logger.error(`Configuration Error: ${flixPatrolConfig.platform} is not a valid platform for FlixPatrol`);
+      flixPatrolTop10Configs = config.get('FlixPatrolTop10') as FlixPatrolTop10Config[];
+      flixPatrolTop10Configs.forEach((flixPatrolTop10Config) => {
+        if (!FlixPatrol.isFlixPatrolTop10Platform(flixPatrolTop10Config.platform)) {
+          logger.error(`Configuration Error: ${flixPatrolTop10Config.platform} is not a valid platform for FlixPatrolTop10`);
           process.exit(1);
         }
-        if (!FlixPatrol.isFlixPatrolLocation(flixPatrolConfig.location)) {
-          logger.error(`Configuration Error: ${flixPatrolConfig.location} is not a valid location for ${flixPatrolConfig.platform} on FlixPatrol`);
+        if (!FlixPatrol.isFlixPatrolTop10Location(flixPatrolTop10Config.location)) {
+          logger.error(`Configuration Error: ${flixPatrolTop10Config.location} is not a valid location for ${flixPatrolTop10Config.platform} on FlixPatrolTop10`);
           process.exit(1);
         }
-        if (flixPatrolConfig.fallback !== false && !FlixPatrol.isFlixPatrolLocation(flixPatrolConfig.fallback)) {
-          logger.error(`Configuration Error: ${flixPatrolConfig.fallback} is not a valid fallback for ${flixPatrolConfig.platform} on FlixPatrol`);
+        if (
+          flixPatrolTop10Config.fallback !== false
+          && !FlixPatrol.isFlixPatrolTop10Location(flixPatrolTop10Config.fallback)
+        ) {
+          logger.error(`Configuration Error: ${flixPatrolTop10Config.fallback} is not a valid fallback for ${flixPatrolTop10Config.platform} on FlixPatrolTop10`);
           process.exit(1);
         }
-        if (!traktPrivacy.includes(flixPatrolConfig.privacy)) {
-          logger.error(`Configuration Error: ${flixPatrolConfig.privacy} is not a valid privacy for ${flixPatrolConfig.platform} on FlixPatrol`);
+        if (!traktPrivacy.includes(flixPatrolTop10Config.privacy)) {
+          logger.error(`Configuration Error: ${flixPatrolTop10Config.privacy} is not a valid privacy for ${flixPatrolTop10Config.platform} on FlixPatrolTop10`);
           process.exit(1);
         }
       });
     } catch (err) {
-      logger.error('Configuration Error: FlixPatrol was not found in configuration file');
+      logger.error('Configuration Error: FlixPatrolTop10 was not found in configuration file');
       process.exit(1);
     }
-    return flixPatrolConfigs;
+    return flixPatrolTop10Configs;
+  }
+
+  public static flixPatrolPopular(): FlixPatrolPopularConfig[] {
+    let flixPatrolPopularConfigs: FlixPatrolPopularConfig[] = [];
+    const traktPrivacy: string[] = ['private', 'link', 'friends', 'public'];
+    try {
+      flixPatrolPopularConfigs = config.get('FlixPatrolPopular') as FlixPatrolPopularConfig[];
+      flixPatrolPopularConfigs.forEach((flixPatrolPopularConfig) => {
+        if (!FlixPatrol.isFlixPatrolPopularPlatform(flixPatrolPopularConfig.platform)) {
+          logger.error(`Configuration Error: ${flixPatrolPopularConfig.platform} is not a valid platform for FlixPatrolPopular`);
+          process.exit(1);
+        }
+        if (!traktPrivacy.includes(flixPatrolPopularConfig.privacy)) {
+          logger.error(`Configuration Error: ${flixPatrolPopularConfig.privacy} is not a valid privacy for ${flixPatrolPopularConfig.platform} on FlixPatrolPopular`);
+          process.exit(1);
+        }
+      });
+    } catch (err) {
+      logger.error('Configuration Error: FlixPatrolPopular was not found in configuration file');
+      process.exit(1);
+    }
+    return flixPatrolPopularConfigs;
   }
 
   public static traktClientId(): string {
