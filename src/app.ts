dda05224ba18defe703b2e15a6034a7acecfe203
Jaunet Nathan <Navino16@users.noreply.github.com>
2024-09-19 16:04:27 +0200

    [FEATURE]: Support most watched Movies and TV Shows (#154)
    * Config for most watched
    
    * Allow array of mostWatched and add year parameter

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 import type { CacheOptions } from './Flixpatrol';
 import { FlixPatrol } from './Flixpatrol';
 import { logger, Utils } from './Utils';
-import type { FlixPatrolPopular, FlixPatrolTop10 } from './Utils/GetAndValidateConfigs';
+import type {FlixPatrolMostWatched, FlixPatrolPopular, FlixPatrolTop10} from './Utils/GetAndValidateConfigs';
 import { GetAndValidateConfigs } from './Utils/GetAndValidateConfigs';
 import type { TraktAPIOptions } from './Trakt';
 import { TraktAPI } from './Trakt';
@@ -14,11 +14,13 @@ const cacheOptions: CacheOptions = GetAndValidateConfigs.getCacheOptions();
 const traktOptions: TraktAPIOptions = GetAndValidateConfigs.getTraktOptions();
 const flixPatrolTop10: FlixPatrolTop10[] = GetAndValidateConfigs.getFlixPatrolTop10();
 const flixPatrolPopulars: FlixPatrolPopular[] = GetAndValidateConfigs.getFlixPatrolPopular();
+const flixPatrolMostWatched: FlixPatrolMostWatched[] = GetAndValidateConfigs.getFlixPatrolMostWatched();
 
 logger.silly(`cacheOptions: ${JSON.stringify(cacheOptions)}`);
 logger.silly(`traktOptions: ${JSON.stringify({...traktOptions, clientId: 'REDACTED', clientSecret: 'REDACTED'})}`);
 logger.silly(`flixPatrolTop10: ${JSON.stringify(flixPatrolTop10)}`);
 logger.silly(`flixPatrolPopulars: ${JSON.stringify(flixPatrolPopulars)}`);
+logger.silly(`flixPatrolMostWatched: ${JSON.stringify(flixPatrolMostWatched)}`);
 
 const flixpatrol = new FlixPatrol(cacheOptions);
 const trakt = new TraktAPI(traktOptions);
@@ -78,6 +80,38 @@ trakt.connect().then(async () => {
       logger.debug(`${popular.platform} shows: ${popularShows}`);
       await trakt.pushToList(popularShows, listName, 'show', popular.privacy);
       logger.info(`List ${listName} updated with ${popularShows.length} new shows`);
+    }
+  }
+
+  for (const mostWatched of flixPatrolMostWatched) {
+    if (mostWatched.enabled) {
+      let listName: string;
+      if (mostWatched.name) {
+        listName = mostWatched.name.toLowerCase().replace(/\s+/g, '-');
+      } else {
+        listName = 'most-watched-netflix';
+        listName = mostWatched.original !== undefined ? `${listName}-original` : listName;
+        listName = mostWatched.premiere !== undefined ? `${listName}-${mostWatched.premiere}` : listName;
+        listName = mostWatched.country !== undefined ? `${listName}-${mostWatched.country}` : listName;
+      }
+
+      if (mostWatched.type === 'movies' || mostWatched.type === 'both') {
+        logger.info('==============================');
+        logger.info(`Getting movies for ${listName}`);
+        const mostWatchedMovies = await flixpatrol.getMostWatched('Movies', mostWatched, trakt);
+        logger.debug(`most-watched movies: ${mostWatchedMovies}`);
+        await trakt.pushToList(mostWatchedMovies, listName, 'movie', mostWatched.privacy);
+        logger.info(`List ${listName} updated with ${mostWatchedMovies.length} new movies`);
+      }
+
+      if (mostWatched.type === 'shows' || mostWatched.type === 'both') {
+        logger.info('==============================');
+        logger.info(`Getting shows for ${listName}`);
+        const mostWatchedShows = await flixpatrol.getMostWatched('TV Shows', mostWatched, trakt);
+        logger.debug(`most-watched shows: ${mostWatchedShows}`);
+        await trakt.pushToList(mostWatchedShows, listName, 'show', mostWatched.privacy);
+        logger.info(`List ${listName} updated with ${mostWatchedShows.length} new shows`);
+      }
     }
   }
 });
